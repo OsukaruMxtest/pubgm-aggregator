e27842fb1c0500576261f6217d58a5d5b996341f
Oscar Brizuela <obz2k12@gmail.com>
2026-03-14 16:47:14 -0600

    fix: dynamic port for cloud deployment

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const app = express();
 app.use(cors());
 app.use(express.json({ limit: "5mb" }));
 
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
 /*
 ================================================
